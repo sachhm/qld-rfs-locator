926000bf47b2a480689bc7f019ec08fdcb31e800
Sachh Ariel Moka <sachhariel.moka@my.jcu.edu.au>
2024-01-10 14:23:23 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# QLD RFS LOCATOR
+Simple app to find the nearest QLD Rural Fire Station given an address.
+
+qld-rfs dataset sourced from [Queensland Government Open Data Portal](https://www.data.qld.gov.au/dataset/rural-fire-stations/resource/e09d0d06-b13e-4e52-b82d-1ce0cf05927c)
---
+++ app.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+
+qld_rfs = pd.DataFrame(
+    {
+        "STATION": "test",
+        "ADDRESS": "Test",
+        "LOCALITY": "TEst",
+        "ALTADDRESS": "Test",
+        "LONG_GDA94": 123,
+        "LAT_GDA94": 123,
+        "LONG_GDA20": 'a123',
+        "LAT_GDA20": "asdasd",
+    }
+)
+
+qld_rfs
