1f58520e301fd14693b562eae32e25c95dd0ddff
Sachh Ariel Moka <sachhariel.moka@my.jcu.edu.au>
2024-01-10 20:56:23 +1000

    Organise app.py into functions

--- app.py
+++ app.py
@@ -1,18 +1,44 @@
 import pandas as pd
-import requests
+import requests, json
 
-# read data as dataframe
-qld_rfs = pd.read_csv("data/qld-rfs.csv")
+# Constants
+STATE = "Queensland"
+QLD_RFS = pd.read_csv("data/qld-rfs.csv")
 
-street_no = float(input("Enter your street number "))
-street = input("Enter your street ")
-locality = input("Enter your locality ")  
+# Functions
+def main():
+    api_call = get_formatted_api_call()
+    lat, long = extract_lat_long(api_call)
+    closest_rfs_lat = find_closest_value_pandas(lat, QLD_RFS["LAT_GDA20"])
+    closest_rfs_long = find_closest_value_pandas(long, QLD_RFS["LONG_GDA20"])
 
-street = street.replace(" ", "+")
-locality = locality.replace(" ", "+")
+    closest_rfs = QLD_RFS[QLD_RFS["LAT_GDA20"] == closest_rfs_lat]
+    closest_rfs = QLD_RFS[QLD_RFS["LONG_GDA20"] == closest_rfs_long]
+    
+    print(closest_rfs)
 
+def get_formatted_api_call():
+    street_no = float(input("Enter your street number "))
+    street = input("Enter your street ")
+    locality = input("Enter your locality ")  
 
-geocoding_request_url = f'https://photon.komoot.io/api/?q={street_no}+{street}+{locality}'
+    street = street.replace(" ", "+")
+    locality = locality.replace(" ", "+")
 
-geocoding_request = requests.get(geocoding_request_url)
-print(geocoding_request_url)
+    geocoding_request_url = f'https://photon.komoot.io/api/?q={street_no}+{street}+{locality}+{STATE}&limit=1'
+    return geocoding_request_url
+
+def extract_lat_long(geocoding_request_url):
+    geocoding_response = requests.get(geocoding_request_url).json()
+    coordinates = geocoding_response['features'][0]['geometry']['coordinates']
+    lat = coordinates[1]
+    long = coordinates[0] 
+    return lat,long
+
+def find_closest_value_pandas(target, series):
+    closest_index = (series - target).abs().idxmin()
+    closest_value = series.loc[closest_index]
+    return closest_value
+    
+
+main()
