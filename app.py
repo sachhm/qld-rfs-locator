2ba9d686d71302ddd813e9c6191a62cc3bb08013
Sachh Ariel Moka <sachhariel.moka@my.jcu.edu.au>
2024-01-11 10:52:01 +1000

    Complete basic web app (still need to work on finding closest rfs algorithm)

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
-from flask import render_template, url_for
+from flask import render_template, url_for, request
+import qld_rfs
 
 app = Flask(__name__)
 
@@ -7,6 +8,25 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template("home.html")
+
+@app.route('/locate', methods=['POST'])
+def locate():
+    street_no = float(request.form['street_no'])
+    street = request.form['street']
+    locality = request.form['locality']
+
+    # Call your geocoding and fire station functions
+    geocoding_request_url = qld_rfs.make_api_request(street_no, street, locality)
+    lat, long = qld_rfs.extract_lat_long(geocoding_request_url)
+    closest_rfs_lat = qld_rfs.find_closest_value_pandas(lat, qld_rfs.QLD_RFS["LAT_GDA20"])
+    closest_rfs_long = qld_rfs.find_closest_value_pandas(long, qld_rfs.QLD_RFS["LONG_GDA20"])
+
+    closest_rfs = qld_rfs.QLD_RFS[qld_rfs.QLD_RFS["LAT_GDA20"] == closest_rfs_lat]
+    closest_rfs = qld_rfs.QLD_RFS[qld_rfs.QLD_RFS["LONG_GDA20"] == closest_rfs_long]
+
+    return render_template("home.html", closest_rfs=closest_rfs)
+
+
 
 @app.route('/about')
 def about():
--- qld_rfs.py
+++ qld_rfs.py
@@ -7,8 +7,9 @@ QLD_RFS = pd.read_csv("data/qld-rfs.csv")
 
 # Functions
 def main():
-    api_call = get_formatted_api_call()
-    lat, long = extract_lat_long(api_call)
+    street_no, street, locality = get_formatted_input()
+    geocoding_request_url = make_api_request(street_no, street, locality)
+    lat, long = extract_lat_long(geocoding_request_url)
     closest_rfs_lat = find_closest_value_pandas(lat, QLD_RFS["LAT_GDA20"])
     closest_rfs_long = find_closest_value_pandas(long, QLD_RFS["LONG_GDA20"])
 
@@ -17,11 +18,14 @@ def main():
     
     print(closest_rfs)
 
-def get_formatted_api_call():
+def get_formatted_input():
     street_no = float(input("Enter your street number "))
     street = input("Enter your street ")
     locality = input("Enter your locality ")  
 
+    return street_no, street, locality
+
+def make_api_request(street_no, street, locality):
     street = street.replace(" ", "+")
     locality = locality.replace(" ", "+")
 
