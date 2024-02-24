498c5d047361cf2a5f536897395327520aad33be
sachhm <sachhmoka@gmail.com>
2024-02-24 22:55:08 +1000

    Refactor CSS, update site layout

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import render_template, url_for, request
 import qld_rfs
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static/')
 
 @app.route('/')
 def home():
